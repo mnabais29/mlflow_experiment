1ae76b68820780b927c160b9fe106cb65b35cc7c
mnabais29 <margaridabmnabais@gmail.com>
2020-12-06 22:13:44 +0000

    finished main code and prepared yaml files

--- train.py
+++ train.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
+import argparse
+import warnings
 import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+
 
 import mlflow
 import mlflow.sklearn
 
 def eval_metrics(actual,pred):
-    f1 = f1_score(actual, pred)
+    f1 = f1_score(actual, pred, average='weighted')
     acc = accuracy_score(actual,pred)
     roc_auc = roc_auc_score(actual, pred)
     return f1,acc,roc_auc
@@ -18,12 +22,29 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(42)
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_estimators','-n_est', type=int)
+    parser.add_argument('--max_depth','-md', type=int)
+    parser.add_argument('--max_features','-mf', type=str)
+    parser.add_argument('--random_state','-rs', type=int)
+    parser.add_argument('--n_jobs','-nj', type=int)
+
+    args = parser.parse_args()
+
+    n_estimators = args.n_estimators
+    max_depth = args.max_depth
+    max_features = args.max_features
+    random_state = args.random_state
+    n_jobs = args.n_jobs
+
+    params = {"n_estimators": n_estimators, "max_depth": max_depth, "max_features": max_features, "random_state": random_state, "n_jobs": n_jobs}
+
     # Read the wine-quality csv file from the URL
     #csv_url = (
     #    "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
     #)
     try:
-        data = pd.read_csv('..\mlflow_experiment\winequality-red.csv',sep=";") #pd.read_csv(csv_url, sep=";")
+        data = pd.read_csv('..\mlflow_experiment\winequality-red.csv') #pd.read_csv(csv_url, sep=";")
     except Exception as e:
         logger.exception(
             "Unable to download training & test CSV, check your internet connection. Error: %s", e
@@ -32,21 +53,26 @@ if __name__ == "__main__":
     x = data.drop('quality', axis=1)
     y = data['quality']
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state= random_state)
 
-    with mlflow.start_run(experiment_id=RFC):
-        rfc = RandomForestClassifier(n_estimators=200)
+    with mlflow.start_run():
+
+        mlflow.sklearn.autolog()
+        rfc = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features, random_state = random_state, n_jobs = n_jobs)
         rfc.fit(x_train,y_train)
 
         prediction = rfc.predict(x_test)
 
         (f1,acc,roc_auc) = eval_metrics(y_test,prediction)
 
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
+        mlflow.log_metric("f1", f1)
+        mlflow.log_metric("acc", acc)
+        mlflow.log_metric("roc_auc", roc_auc)
 
+        # Model registry does not work with file store
+        #if tracking_url_type_store != "file":
             # Register the model
-            mlflow.sklearn.log_model(rfc, "model", registered_model_name="RFCWineModel")
-        else:
-            mlflow.sklearn.log_model(rfc, "model")
+        #    mlflow.sklearn.log_model(rfc, "model", registered_model_name="RFCWineModel")
+        #else:
+        #    mlflow.sklearn.log_model(rfc, "model")
 
