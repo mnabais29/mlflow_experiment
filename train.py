c7c97c580fa9903e842d0e6b25fbee7f252a40ed
mnabais29 <margaridabmnabais@gmail.com>
2020-12-07 16:42:24 +0000

    configuration file changed and first experiment concluded

--- train.py
+++ train.py
@@ -6,17 +6,18 @@ import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import classification_report, accuracy_score, f1_score
+from sklearn.metrics import roc_auc_score
 
 
 import mlflow
 import mlflow.sklearn
 
 def eval_metrics(actual,pred):
-    f1 = f1_score(actual, pred, average='weighted')
+    f1 = f1_score(actual, pred, average='macro')
     acc = accuracy_score(actual,pred)
-    roc_auc = roc_auc_score(actual, pred)
-    return f1,acc,roc_auc
+    #roc_auc = roc_auc_score(actual, pred, multi_class="ovo", average="macro")
+    return f1,acc
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
@@ -55,19 +56,26 @@ if __name__ == "__main__":
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state= random_state)
 
+    print(y)
+    print(y_test)
+    print(y_train)
+
     with mlflow.start_run():
 
         mlflow.sklearn.autolog()
         rfc = RandomForestClassifier(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features, random_state = random_state, n_jobs = n_jobs)
         rfc.fit(x_train,y_train)
 
-        prediction = rfc.predict(x_test)
+        y_pred = rfc.predict(x_test)
 
-        (f1,acc,roc_auc) = eval_metrics(y_test,prediction)
+        print(classification_report(y_test, y_pred))
+
+        (f1,acc) = eval_metrics(y_test,y_pred)
 
         mlflow.log_metric("f1", f1)
         mlflow.log_metric("acc", acc)
-        mlflow.log_metric("roc_auc", roc_auc)
+
+        #mlflow.log_metric("roc_auc", roc_auc)
 
         # Model registry does not work with file store
         #if tracking_url_type_store != "file":
